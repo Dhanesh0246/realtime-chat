62fe3441806687165c17bbc7dc1f74555efeebea
Dhanesh0246 <143717230+Dhanesh0246@users.noreply.github.com>
2025-06-02 20:52:09 +0530

    Update config.js

--- server/constants/config.js
+++ server/constants/config.js
@@ -1,9 +1,7 @@
-const corsOptions = {
-  origin: [
-    process.env.CLIENT_URL,"http://localhost:5173"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+ export const corsOptions = {
+  origin: "*",  
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
 const CHAT_TOKEN = "chat-token";
