2883d295b420857768df829988c6cc3002389215
Dhanesh0246 <143717230+Dhanesh0246@users.noreply.github.com>
2025-06-02 21:34:09 +0530

    Update user.js

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -332,7 +332,7 @@ const mail = async (req, res) => {
       service: "gmail",
       auth: {
         user: "gldhanesh22@gmail.com",
-        pass: "vzkrnsbrhzhdihvf",
+        pass: "pcyp zcjb hcni cbvi",
       },
     });
 
